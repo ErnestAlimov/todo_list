c9891e3b26d5af00d3a2c38f3688c9a89d6a9257
ErnestAlimov <141016871+ErnestAlimov@users.noreply.github.com>
2023-08-11 11:13:13 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	dbmanager "todo_list/internal/db-manager"
+	"todo_list/internal/handlers"
+	"todo_list/internal/service"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	// Подключение к базе данных MongoDB
+
+	dbManager := dbmanager.NewDbManager("localhost", "27017", "test", "test")
+	taskService := service.NewTaskService(dbManager)
+
+	handlers := handlers.NewHandler(taskService)
+
+	// Создание роутера с использованием Gin
+	router := gin.Default()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	v1 := router.Group("/api/v1")
+	{
+		//Обработчики
+		v1.POST("/todo-list/tasks", handlers.CreateTask)
+		v1.PUT("/todo-list/tasks/:id", handlers.UpdateTask)
+		v1.DELETE("/todo-list/tasks/:id", handlers.DeleteTask)
+		v1.PUT("/todo-list/tasks/:id/done", handlers.MarkTaskDone)
+		v1.GET("/todo-list/tasks", handlers.GetTasksByStatus)
+	}
+
+	//Запуск сервера
+	router.Run(":8080")
+}
